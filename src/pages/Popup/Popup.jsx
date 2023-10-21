c5234d4ed80f023175d31122b55cd039ddb5080e
Jtawfik04 <johntawfik106@gmail.com>
2023-10-21 03:10:28 -0400

    remove newtab and add pop-up

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
+import React, { useState } from 'react';
 import './Popup.css';
 
 const Popup = () => {
+  const [language, setLanguage] = useState('English'); // default language
+  const [difficulty, setDifficulty] = useState('medium'); // default difficulty
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
+        <div className="DropdownWrapper">
+          <select
+            className="Drop-down"
+            value={language}
+            onChange={(e) => setLanguage(e.target.value)}
+          >
+            <option value="English">English</option>
+            <option value="Spanish">Spanish</option>
+            <option value="French">French</option>
+            <option value="German">German</option>
+            <option value="Chinese">Chinese</option>
+          </select>
+        </div>
+
+        <div className="Slider">
+          <h3>Difficulty: {difficulty}</h3>
+          <input
+            type="range"
+            min="1"
+            max="3"
+            value={difficulty === 'easy' ? 1 : difficulty === 'medium' ? 2 : 3}
+            onChange={(e) => {
+              const value = e.target.value;
+              if (value === '1') setDifficulty('easy');
+              else if (value === '2') setDifficulty('medium');
+              else if (value === '3') setDifficulty('hard');
+            }}
+          />
+        </div>
       </header>
     </div>
   );
